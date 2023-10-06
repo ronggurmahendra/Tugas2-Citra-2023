bb238d95b7e20bc429e48436fb9911d37f68223f
ronggurmahendra <ronggur.mwp@gmail.com>
2023-10-06 21:24:22 +0700

    spek 5 6

--- main.m
+++ main.m
@@ -4,7 +4,9 @@ function main()
     clear all;
     % my_conv_test()
     % my_smoothing_test()
-    spek3()
+    % spek3()
+    % spek4()
+    spek5()
 end
 % spek 1
 
@@ -70,16 +72,6 @@ function my_conv_test()
     imshow(hasilKonvolusiMatlab)
 end
 
-% Spek 2
-% Tulislah program Matlab untuk melakukan image smoothing (pada citra yang mengandung derau)
-% atau blurring pada citra dalam:
-% - ranah spasial menggunakan mean filter n x n dan gaussian filter n x n. Gunakan fungsi
-% konvolusi yang telah anda kerjakan pada poin 1.
-% - Ranah frekuensi menggunakan low-pass filter ILPF, GLPF, dan BLPF.
-% Citra yang digunakan adalah citra grayscale dan citra berwarna. Uji program dengan
-% menggunakan tiga contoh citra grayscale dan tiga contoh citra berwarna berikut dan dua citra
-% tambahan (grayscale dan berwarna) yang Anda cari sendiri.
-
 function result = image_mean_grey(image,n)
     mean_mask = ones(n) / (n * n);
     result = my_grey_conv(image, mean_mask);
@@ -114,15 +106,6 @@ function my_smoothing_test()
     % print(mask2)
     figure;
     imshow(img_input)
-    % disp(length("1.jpg"))
-    % isGreyscale = endsWith(lower(filename), '.bmp');
-
-    % % Extract the last 3 characters
-    % lastThreeChars = filename(length(filename)-2:length(filename));
-    
-    % % Check if it's "jpg" (case insensitive)
-    % isGreyscale = strcmpi(lastThreeChars, 'bmp');
-
     % if isGreyscale
     %     img_result = uint8(image_mean_grey(double(img_input), 3));
     % else
@@ -134,80 +117,6 @@ function my_smoothing_test()
     figure;
     imshow(img_result)
 end
-
-% Spek3
-% Tulislah program Matlab untuk melakukan penapisan citra dalam ranah frekuensi menggunakan
-% high-pass filter IHPF, GHPF, dan BHPF. Citra yang digunakan adalah citra grayscale dan citra
-% berwarna.
-% Uji program dengan menggunakan contoh citra berikut dan dua citra tambahan yang anda cari
-% % sendiri
-% function spek3()
-%     % Baca citra grayscale
-%     filename = "1.bmp"
-%     grayscale_image = imread(filename);
-%     % Konversi ke domain frekuensi
-%     grayscale_image_fft = fftshift(fft2(grayscale_image));
-
-%     % Baca citra berwarna
-%     filename = "1.jpg"
-%     color_image = imread(filename);
-%     % Konversi ke domain frekuensi
-%     color_image_fft = fftshift(fft2(rgb2gray(color_image)));
-
-%     % Parameter filter high-pass
-%     D0 = 50; % Parameter jarak cutoff
-%     n = 2;   % Orde filter Butterworth
-
-%     % Filter IHPF
-%     IHPF = ones(size(grayscale_image_fft));
-%     [m, n] = size(IHPF);
-%     center_x = round(m / 2);
-%     center_y = round(n / 2);
-%     radius = D0;
-%     for i = 1:m
-%         for j = 1:n
-%             distance = sqrt((i - center_x)^2 + (j - center_y)^2);
-%             if distance <= radius
-%                 IHPF(i, j) = 0;
-%             end
-%         end
-%     end
-
-%     % Filter GHPF
-%     GHPF = 1 - exp(-(grayscale_image_fft.^2) / (2 * D0^2));
-
-%     % Filter BHPF
-%     % BHPF = 1 ./ (1 + (D0 ./ distance_matrix(grayscale_image)).^(2 * n));
-
-%     % Terapkan filter pada citra grayscale
-%     filtered_grayscale_IHPF = ifft2(ifftshift(grayscale_image_fft .* IHPF));
-%     filtered_grayscale_GHPF = ifft2(ifftshift(grayscale_image_fft .* GHPF));
-%     % filtered_grayscale_BHPF = ifft2(ifftshift(grayscale_image_fft .* BHPF));
-
-%     % Tampilkan citra hasil
-%     imshow(filtered_grayscale_IHPF, []);
-%     title('Filtered Grayscale Image - IHPF');
-%     imshow(filtered_grayscale_GHPF, []);
-%     title('Filtered Grayscale Image - GHPF');
-%     % imshow(filtered_grayscale_BHPF, []);
-%     % title('Filtered Grayscale Image - BHPF');
-
-%     % Terapkan filter pada citra berwarna
-%     filtered_color_IHPF = ifft2(ifftshift(color_image_fft .* IHPF));
-%     filtered_color_GHPF = ifft2(ifftshift(color_image_fft .* GHPF));
-%     % filtered_color_BHPF = ifft2(ifftshift(color_image_fft .* BHPF));
-
-%     % Tampilkan citra hasil
-%     figure;
-%     imshow(uint8(filtered_color_IHPF));
-%     title('Filtered Color Image - IHPF');
-%     figure;
-%     imshow(uint8(filtered_color_GHPF));
-%     title('Filtered Color Image - GHPF');
-%     % figure;
-%     % imshow(uint8(filtered_color_BHPF));
-%     % title('Filtered Color Image - BHPF');
-% end
 
 function spek3()
     % Baca citra grayscale
@@ -228,10 +137,30 @@ function spek3()
     % Buat filter BHPF
     % bhp_filter = fspecial('butter', 2, 0.5);
 
-    % Terapkan filter pada citra grayscale
-    filtered_gray_ihpf = imfilter(gray_image, ihpf_filter);
-    filtered_gray_ghpf = imfilter(gray_image, ghpf_filter);
-    % filtered_gray_bhp = imfilter(gray_image, bhp_filter);
+    [M, N] = size(color_image); 
+    % tranform fourier
+    FT_img = fft2(double(color_image)); 
+    n = 2
+    D0 = 10;
+
+    u = 0:(M-1); 
+    v = 0:(N-1); 
+    idx = find(u > M/2); 
+    u(idx) = u(idx) - M; 
+    idy = find(v > N/2); 
+    v(idy) = v(idy) - N; 
+     
+    [V, U] = meshgrid(v, u); 
+
+
+    D = sqrt(U.^2 + V.^2); 
+
+    H = 1./(1 + (D0./D).^(2*n)); 
+
+    G = H.*FT_img; 
+    %Inverse fouriser transform
+    img_result = real(ifft2(double(G)));  
+
 
     % Terapkan filter pada citra berwarna
     filtered_color_ihpf = imfilter(color_image, ihpf_filter);
@@ -281,6 +210,41 @@ end
 % Spek 4
 % Tulislah program Matlab untuk melakukan penapisan citra dalam ranah frekuensi untuk
 % menghasilkan citra yang lebih terang seperti di bawah ini. Pikirkan bagaimana car
+function spek4()
+    % Baca citra grayscale
+    gray_image = imread('image-017.bmp');
+    
+    % Baca citra berwarna
+    color_image = imread('image-019.jpg');
+    h_size = 10
+    Sigma = 10 
+    A = 3
+    GLPF = fspecial('gaussian',h_size,Sigma);
+    GLPF_Image = imfilter(color_image,GLPF);
+    
+    High_pass_filter = color_image - GLPF_Image;
+
+    Unsharpened_Image = (1*color_image) + High_pass_filter;
+
+    HFBF_Image = (A-1)*color_image + color_image - GLPF_Image;
+    figure(),imshow(color_image),
+    title('Original Image')
+    figure(),imshow(GLPF_Image);
+    title('Gaussian Low Pass Filtered Image');
+    figure(),imshow(Unsharpened_Image);
+    title('Unsharpened Image');
+    figure(),imshow(HFBF_Image);
+    title('High Frequency Boosted Image');
+    
+    % figure;
+    % subplot(1, 2, 1);
+    % imshow(gray_image);
+    % title('Citra Asli');
+
+    % subplot(1, 2, 2);
+    % imshow(filteredImage);
+    % title('Citra Terang');
+end
 
 % Spek 5 
 % Buatlah program Matlab untuk menambahkan derau salt and pepper pada citra (grayscale dan
@@ -289,6 +253,339 @@ end
 % alpha-trimmed mean filter. Tidak boleh menggunakan fungsi medfilt2 di dalam Matlab. Uji
 % program dengan menggunakan contoh citra berikut dan dua citra tambahan yang anda cari
 % sendiri.
+function outputImage = min_filter(inputImage, kernelSize)
+    % Ensure kernelSize is odd
+    if mod(kernelSize, 2) == 0
+        error('Kernel size must be odd');
+    end
+    
+    % Get image dimensions
+    [rows, cols] = size(inputImage);
+    
+    % Initialize the output image
+    outputImage = zeros(rows, cols);
+    
+    % Calculate the radius of the kernel
+    radius = (kernelSize - 1) / 2;
+    
+    for i = 1:rows
+        for j = 1:cols
+            % Get the neighborhood of the current pixel
+            neighborhood = inputImage(max(1, i - radius):min(rows, i + radius), ...
+                                      max(1, j - radius):min(cols, j + radius));
+            
+            % Apply the minimum filter operation
+            outputImage(i, j) = min(neighborhood(:));
+        end
+    end
+end
+
+function outputImage = max_filter(inputImage, kernelSize)
+    % Ensure kernelSize is odd
+    if mod(kernelSize, 2) == 0
+        error('Kernel size must be odd');
+    end
+    
+    % Get image dimensions
+    [rows, cols] = size(inputImage);
+    
+    % Initialize the output image
+    outputImage = zeros(rows, cols);
+    
+    % Calculate the radius of the kernel
+    radius = (kernelSize - 1) / 2;
+    
+    for i = 1:rows
+        for j = 1:cols
+            % Get the neighborhood of the current pixel
+            neighborhood = inputImage(max(1, i - radius):min(rows, i + radius), ...
+                                      max(1, j - radius):min(cols, j + radius));
+            
+            % Apply the maximum filter operation
+            outputImage(i, j) = max(neighborhood(:));
+        end
+    end
+end
+function outputImage = median_filter(inputImage, kernelSize)
+    % Ensure kernelSize is odd
+    if mod(kernelSize, 2) == 0
+        error('Kernel size must be odd');
+    end
+    
+    % Get image dimensions
+    [rows, cols] = size(inputImage);
+    
+    % Initialize the output image
+    outputImage = zeros(rows, cols);
+    
+    % Calculate the radius of the kernel
+    radius = (kernelSize - 1) / 2;
+    
+    for i = 1:rows
+        for j = 1:cols
+            % Get the neighborhood of the current pixel
+            neighborhood = inputImage(max(1, i - radius):min(rows, i + radius), ...
+                                      max(1, j - radius):min(cols, j + radius));
+            
+            % Apply the median filter operation
+            outputImage(i, j) = median(neighborhood(:));
+        end
+    end
+end
+
+function outputImage = arithmetic_mean_filter(inputImage, kernelSize)
+    % Ensure kernelSize is odd
+    if mod(kernelSize, 2) == 0
+        error('Kernel size must be odd');
+    end
+    
+    % Get image dimensions
+    [rows, cols] = size(inputImage);
+    
+    % Initialize the output image
+    outputImage = zeros(rows, cols);
+    
+    % Calculate the radius of the kernel
+    radius = (kernelSize - 1) / 2;
+    
+    for i = 1:rows
+        for j = 1:cols
+            % Get the neighborhood of the current pixel
+            neighborhood = inputImage(max(1, i - radius):min(rows, i + radius), ...
+                                      max(1, j - radius):min(cols, j + radius));
+            
+            % Calculate the average value in the neighborhood
+            outputImage(i, j) = mean(neighborhood(:));
+        end
+    end
+end
+
+function outputImage = geometric_filter(inputImage, kernelSize, angle_degrees)
+    % Ensure kernelSize is odd
+    if mod(kernelSize, 2) == 0
+        error('Kernel size must be odd');
+    end
+    
+    % Get image dimensions
+    [rows, cols] = size(inputImage);
+    
+    % Initialize the output image
+    outputImage = zeros(rows, cols);
+    
+    % Calculate the radius of the kernel
+    radius = (kernelSize - 1) / 2;
+    
+    % Convert the angle from degrees to radians
+    angle_radians = deg2rad(angle_degrees);
+    
+    for i = 1:rows
+        for j = 1:cols
+            % Get the neighborhood of the current pixel
+            neighborhood = inputImage(max(1, i - radius):min(rows, i + radius), ...
+                                      max(1, j - radius):min(cols, j + radius));
+            
+            % Perform a geometric operation (e.g., rotation) on the neighborhood
+            rotated_neighborhood = imrotate(neighborhood, angle_degrees, 'bilinear', 'crop');
+            
+            % Calculate the center of the rotated neighborhood
+            center = floor(size(rotated_neighborhood) / 2) + 1;
+            
+            % Assign the central pixel value of the rotated neighborhood to the output image
+            outputImage(i, j) = rotated_neighborhood(center(1), center(2));
+        end
+    end
+end
+function outputImage = harmonic_mean_filter(inputImage, kernelSize)
+    % Ensure kernelSize is odd
+    if mod(kernelSize, 2) == 0
+        error('Kernel size must be odd');
+    end
+    
+    % Get image dimensions
+    [rows, cols] = size(inputImage);
+    
+    % Initialize the output image
+    outputImage = zeros(rows, cols);
+    
+    % Calculate the radius of the kernel
+    radius = (kernelSize - 1) / 2;
+    
+    for i = 1:rows
+        for j = 1:cols
+            % Get the neighborhood of the current pixel
+            neighborhood = inputImage(max(1, i - radius):min(rows, i + radius), ...
+                                      max(1, j - radius):min(cols, j + radius));
+            
+            % Calculate the harmonic mean of the neighborhood
+            reciprocal_neighborhood = 1 ./ double(neighborhood);
+            harmonic_mean = kernelSize^2 / sum(reciprocal_neighborhood(:));
+            
+            % Assign the harmonic mean to the output image
+            outputImage(i, j) = harmonic_mean;
+        end
+    end
+end
+function outputImage = contraharmonic_mean_filter(inputImage, kernelSize, q)
+    % Ensure kernelSize is odd
+    if mod(kernelSize, 2) == 0
+        error('Kernel size must be odd');
+    end
+    
+    % Get image dimensions
+    [rows, cols] = size(inputImage);
+    
+    % Initialize the output image
+    outputImage = zeros(rows, cols);
+    
+    % Calculate the radius of the kernel
+    radius = (kernelSize - 1) / 2;
+    
+    for i = 1:rows
+        for j = 1:cols
+            % Get the neighborhood of the current pixel
+            neighborhood = inputImage(max(1, i - radius):min(rows, i + radius), ...
+                                      max(1, j - radius):min(cols, j + radius));
+            
+            % Calculate the numerator and denominator of the contraharmonic mean
+            numerator = sum(neighborhood(:) .^ (q + 1));
+            denominator = sum(neighborhood(:) .^ q);
+            
+            % Compute the contraharmonic mean
+            if denominator == 0
+                outputImage(i, j) = 0;
+            else
+                outputImage(i, j) = numerator / denominator;
+            end
+        end
+    end
+end
+function outputImage = midpoint_filter(inputImage, kernelSize)
+    % Ensure kernelSize is odd
+    if mod(kernelSize, 2) == 0
+        error('Kernel size must be odd');
+    end
+    
+    % Get image dimensions
+    [rows, cols] = size(inputImage);
+    
+    % Initialize the output image
+    outputImage = zeros(rows, cols);
+    
+    % Calculate the radius of the kernel
+    radius = (kernelSize - 1) / 2;
+    
+    for i = 1:rows
+        for j = 1:cols
+            % Get the neighborhood of the current pixel
+            neighborhood = inputImage(max(1, i - radius):min(rows, i + radius), ...
+                                      max(1, j - radius):min(cols, j + radius));
+            
+            % Calculate the midpoint (median) value in the neighborhood
+            outputImage(i, j) = median(neighborhood(:));
+        end
+    end
+end
+function outputImage = alpha_trimmed_mean_filter(inputImage, kernelSize, d)
+    % Ensure kernelSize is odd
+    if mod(kernelSize, 2) == 0
+        error('Kernel size must be odd');
+    end
+    
+    % Get image dimensions
+    [rows, cols] = size(inputImage);
+    
+    % Initialize the output image
+    outputImage = zeros(rows, cols);
+    
+    % Calculate the radius of the kernel
+    radius = (kernelSize - 1) / 2;
+    
+    for i = 1:rows
+        for j = 1:cols
+            % Get the neighborhood of the current pixel
+            neighborhood = inputImage(max(1, i - radius):min(rows, i + radius), ...
+                                      max(1, j - radius):min(cols, j + radius));
+            
+            % Sort the neighborhood pixel values
+            sorted_neighborhood = sort(neighborhood(:));
+            
+            % Exclude d/2 lowest and d/2 highest pixel values
+            trimmed_neighborhood = sorted_neighborhood((d/2 + 1):(end - d/2));
+            
+            % Calculate the mean value of the trimmed neighborhood
+            outputImage(i, j) = mean(trimmed_neighborhood);
+        end
+    end
+end
+
+
+function spek5()
+    % Baca citra grayscale
+    gray_image = imread('image-021.bmp');
+    gray_myImage = imread('myImage.bmp');
+
+    
+    % Baca citra berwarna
+    color_image = imread('image-022.jpg');
+    color_myImage = imread('myImage.jpg');
+    
+    kernelSize = 3
+    color_image_saltnpepper = imnoise( color_image ,'salt & pepper')
+
+    min_denoised_image = min_filter(color_image_saltnpepper,kernelSize);
+    max_denoised_image = max_filter(color_image_saltnpepper,kernelSize);
+    median_denoised_image = median_filter(color_image_saltnpepper,kernelSize);
+    arithmetic_mean_denoised_image = arithmetic_mean_filter(color_image_saltnpepper,kernelSize);
+    geometric_denoised_image = geometric_filter(color_image_saltnpepper,kernelSize,45);
+    harmonic_mean_denoised_image = harmonic_mean_filter(color_image_saltnpepper,kernelSize);
+    contraharmonic_mean_denoised_image = contraharmonic_mean_filter(color_image_saltnpepper,kernelSize, 1.5);
+    midpoint_denoised_image = midpoint_filter(color_image_saltnpepper,kernelSize);
+    alpha_trimmed_mean_denoised_image = alpha_trimmed_mean_filter(color_image_saltnpepper,kernelSize,2);
+
+    figure;
+    subplot(6, 2, 1);
+    imshow(color_image);
+    title('Original Asli');
+
+    subplot(6, 2, 2);
+    imshow(color_image_saltnpepper);
+    title('Citra saltnpepper');
+
+    subplot(6, 2, 3);
+    imshow(color_image_saltnpepper);
+    title('Citra min_denoised_image');
+
+    subplot(6, 2, 4);
+    imshow(color_image_saltnpepper);
+    title('Citra max_denoised_image');
+
+    subplot(6, 2, 5);
+    imshow(color_image_saltnpepper);
+    title('Citra median_denoised_image');
+
+    subplot(6, 2, 6);
+    imshow(color_image_saltnpepper);
+    title('Citra arithmetic_mean_denoised_image');
+
+    subplot(6, 2, 7);
+    imshow(color_image_saltnpepper);
+    title('Citra geometric_denoised_image');
+
+    subplot(6, 2, 8);
+    imshow(color_image_saltnpepper);
+    title('Citra harmonic_mean_denoised_image');
+
+    subplot(6, 2, 9);
+    imshow(color_image_saltnpepper);
+    title('Citra contraharmonic_mean_denoised_image');
+
+    subplot(6, 2, 10);
+    imshow(color_image_saltnpepper);
+    title('Citra midpoint_denoised_image');
+    subplot(6, 2, 11);
+    imshow(color_image_saltnpepper);
+    title('Citra alpha_trimmed_mean_denoised_image');
+end
 
 % Spek 6
 % Pikirkan bagaimana cara menghilangan derau periodik pada citra berikut, lalu tulislah program
